bf0731e8a3decd88a0c6328613f49c777983d4bc
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2026-05-27 18:57:10 -0400

    Feat: anotacoes sobre o metodo main.

--- java-study-notes/src/Main.java
+++ java-study-notes/src/Main.java
@@ -1,7 +1,15 @@
+
+// O código precisa estar dentro de uma classe, a classe main é a classe principal do projeto onde a aplicação será iniciada
+
+// A origem do projeto está dentro da nossa classe main, sendo o ponto de partida do nosso programa
+
+// Quando executamos um programa em java, a primeira coisa que o compilador procura é o metodo com a assinatura main.
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Olá mundo!");
+        System.out.println("Hello World!");
 
 
 
--- java-study-notes/src/anotacoes/EstruturasSequenciais/TiposPrimitivos.java
+++ java-study-notes/src/anotacoes/EstruturasSequenciais/TiposPrimitivos.java
@@ -3,5 +3,7 @@ package anotacoes.EstruturasSequenciais;
 public class TiposPrimitivos {
     public static void main(String[] args) {
 
+
+
     }
 }
