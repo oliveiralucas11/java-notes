5fb88bcf0192886c5ac0529b54887d467ee71628
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2026-05-28 18:21:02 -0300

    feat: entrada e saida de dados usando scanner.

---
+++ java-study-notes/src/anotacoes/EstruturasSequenciais/EntradaSaidaDados.java
@@ -0,0 +1,32 @@
+package anotacoes.EstruturasSequenciais;
+
+import java.util.Scanner;
+
+public class EntradaSaidaDados {
+    public static void main(String[] args) {
+
+        // Saida de dados
+        // Usamos o System.out.print(), System.out.println() e System.out.printf() para imprimir dados na tela
+        // System.out.print() - Imprime sem pular a linha
+        System.out.println("Olá eu faco impressao e pulo a linha");
+        // System.out.println() - Imprime e pula pra proxima linha
+        System.out.print("Eu faco a impressao na mesma linha");
+        // System.out.printf() - Imprime formatado
+        System.out.printf("Eu faco a impressao formatado");
+
+        // Entrada de dados
+        // Usamos a classe Scanner para ler dados do teclado.
+
+        Scanner scanner = new Scanner(System.in); // Estamos instanciando um novo objeto scanner na nossa classe scanner
+        // Scanner permite atribuirmos valores em tempo de execucao
+        System.out.println("Olá, digite o seu nome: " );
+        String nome3 = scanner.nextLine(); // A variavel tem um valor atribuido em tempo de execucao e nao estatico.
+        System.out.println("Nome: " + nome3);
+
+        System.out.println("Digite sua idade: ");
+        int idade4 = scanner.nextInt();
+        System.out.println("Idade: " + idade4);
+
+
+    }
+}
