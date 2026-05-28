40fffd19025b6892a6701b008d2fee116d9fc85a
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2026-05-28 16:12:58 -0300

    feat: anotacoes e comentarios com exemplos adicionados sobre operadores aritmeticos

---
+++ java-study-notes/src/anotacoes/EstruturasSequenciais/OperadoresAritmeticos.java
@@ -0,0 +1,47 @@
+package anotacoes.EstruturasSequenciais;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+
+        // Operadores Aritmeticos - Usados pra realizar os calculos matematicos acompanhado de precedencia de Operadores.
+
+        // Adicao +
+        int soma = 2 + 2;
+        System.out.println(soma);
+        int soma1 = 3;
+        int soma2 = 4;
+        int resultado = soma1 + soma2;
+        System.out.println("O resultado é: " + resultado);
+        System.out.println(resultado);
+        // Temos varias formas de fazer a mesma operacao
+
+        // Subtracao -
+        int subtracao = 3 - 3;
+        System.out.println(subtracao);
+        int numero1 = 5;
+        int numero2 = 3;
+        int resultado1 = numero1 - numero2;
+        System.out.println(resultado1);
+        System.out.println("O resultado da subtracao é: " + resultado1);
+
+        // Multiplicacao *
+        int multiplicacao = 4 * 5;
+        System.out.println(multiplicacao);
+        int multi1 = 10;
+        int multi2 = 20;
+        int resultado2 = multi1 * multi2;
+        System.out.println(resultado2);
+        System.out.println("O resultado da multiplicacao é: " + resultado2);
+
+        // Divisao /
+        int divisao = 10 / 2;
+        System.out.println(divisao);
+        int div1 = 10;
+        int div2 = 2;
+        int resultado3 = div1 / div2;
+        System.out.println(resultado3);
+        System.out.println("O resultado da divisao é: " + resultado3);
+
+
+    }
+}
