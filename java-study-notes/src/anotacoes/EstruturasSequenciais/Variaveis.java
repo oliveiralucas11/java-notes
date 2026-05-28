dabf221445fc79055f08c81b8701f208b19b4286
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2026-05-28 15:28:07 -0300

    feat: anotacoes adicionadas explicando e comentando sobre variaveis e sua inicializacao

---
+++ java-study-notes/src/anotacoes/EstruturasSequenciais/Variaveis.java
@@ -0,0 +1,16 @@
+package anotacoes.EstruturasSequenciais;
+
+public class Variaveis {
+    public static void main(String[] args) {
+
+        // Variaveis - sao espacos na memoria do computador onde podemos armazenar dados, podemos variar ela em qualquer momento do codigo alterando o seu valor
+
+        // Para declarar uma variavel especificamos o seu tipo e depois o seu nome ex:
+
+        int idade; // Podemos declarar uma variavel ainda sem valor e defini-lo logo depois.
+        idade = 25; // Dessa forma estamos inicializando a variavel atribuindo valor a ela.
+
+        // Convencoes - Nomes de variaveis devem comecar  com uma letra minuscula e seguir o padrao camelCase.
+        // Ex: idade, salarioMensal, nomeCompleto, ganhoTotal, ganhoMensal
+    }
+}
